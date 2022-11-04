09cd8977f70239261a9ac9187e16cefe9707a5ae
abethel912 <abethel180@gmail.com>
2022-11-04 13:10:06 -0400

    Greeting express application created

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// Make a route '/greeting'that sends a generic greeting to the screen like "Hello, stranger".
+
 const express = require('express')
 const app = express()
 
-
+app.get('/greeting/:name', (request, response) => {
+  response.send(`Hello, ${request.params.name}!`)
+})
 
 app.listen(3000, () => {
   console.log('listening in port 3000')
